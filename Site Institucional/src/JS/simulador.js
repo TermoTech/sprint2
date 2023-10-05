d2b522932cd584d2cd757ca48fab7b96c8ce974f
Gabriel Amaral <gabriel.amaral@sptech.school>
2023-10-04 22:33:42 -0300

    resolvi bug do simulador e atualizei logica do js

--- Site Institucional/src/JS/simulador.js
+++ Site Institucional/src/JS/simulador.js
@@ -23,44 +23,14 @@ function calcula(){
         var prodMin = qtdReais/60;
         var ganhoMin = prodMin*qtdKg;
 
-        section_content.innerHTML += `
-        <div class="values-result" id="div_table">
-            <div class="table-res">
-                <h1 class="table-title">Simulação de perdas</h1>
-                <table class="table-results">
-                    <tr>
-                        <th>Tempo em temperatura inadequada</th>
-                        <th>Perda em <b><u>KG</u></b></th>
-                        <th>Perda em <b><u>R$</u></b></th>
-                    </tr>
-                    <tr>
-                        <td>7 minutos</td>
-                        <td>${(prodMin*7).toFixed(2)}kg</td>
-                        <td>R$${(ganhoMin*7).toFixed(2)}</td>
-                    </tr>
-                    <tr>
-                        <td>15 minutos</td>
-                        <td>${(prodMin*15).toFixed(2)}kg</td>
-                        <td>R$${(ganhoMin*15).toFixed(2)}</td>
-                    </tr>
-                    <tr>
-                        <td>30 minutos</td>
-                        <td>${(prodMin*30).toFixed(2)}kg</td>
-                        <td>R$${(ganhoMin*30).toFixed(2)}</td>
-                    </tr>
-                    <tr>
-                        <td>1 hora e 30 minutos</td>
-                        <td>${(prodMin*90).toFixed(2)}kg</td>
-                        <td>R$${(ganhoMin*90).toFixed(2)}</td>
-                    </tr>
-                </table>
-            </div>
-            <h4 class="txt-finance">
-                Como podemos perceber, em minutos de desatenção você pode ter uma perda considerável.
-                Quer mesmo depender do fator humano?
-            </h4>
-            <button class="btn-chamada" onclick="abrirPopUp()">NÃO!!!</button>
-        </div>
-        `
+        div_table.style.display= "flex";
+        td1.innerHTML = `${(prodMin*7).toFixed(2)}kg`;
+        td2.innerHTML = `R$${(ganhoMin*7).toFixed(2)}`;
+        td3.innerHTML = `${(prodMin*15).toFixed(2)}kg`;
+        td4.innerHTML = `R$${(ganhoMin*15).toFixed(2)}`;
+        td5.innerHTML = `${(prodMin*30).toFixed(2)}kg`;
+        td6.innerHTML = `R$${(ganhoMin*30).toFixed(2)}`;
+        td7.innerHTML = `${(prodMin*90).toFixed(2)}kg`;
+        td8.innerHTML = `R$${(ganhoMin*90).toFixed(2)}`;
     }
 };
