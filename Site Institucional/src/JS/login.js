e335937f9555788053e8fc06b5f4949ede70f224
thawan bruno <thawan.rodrigues@sptech.school>
2023-10-12 20:26:18 -0300

    Alterando a lógica do login.js

--- Site Institucional/src/JS/login.js
+++ Site Institucional/src/JS/login.js
@@ -2,9 +2,28 @@ function fazerLogin(){
     var user = ipt_user.value;
     var senha = ipt_senha.value;
 
-    if(user.toUpperCase() == "CARAMICO" && senha == "ipt1234"){
-        window.location = "dashboard.html";
+    let users = ['caramico.herreira@basic.admin', 'caramico.herreira@premium.admin', 'caramico.herreira@basic.funcionaria', 'caramico.herreira@premium.funcionaria'];
+
+    let senhas = ['ipt1230', 'ipt1231', 'ipt1232', 'ipt1233'];
+    if(users.indexOf(user) >= 0 && senha == senhas[0]){
+            window.location = "../../../dashboard/supervisor/dashboard.html"; 
+        
+    } if(users.indexOf(user) >= 0 && senha == senhas[1]){
+        window.location = "../../../dashboard/supervisor/dashboard.html"; 
+    
+    } if(users.indexOf(user) >= 0 && senha == senhas[2]){
+        window.location = "../../../dashboard/supervisor/dashboard.html"; 
+    
+    } if(users.indexOf(user) >= 0 && senha == senhas[3]){
+        window.location = "../../../dashboard/supervisor/dashboard.html"; 
+    
     } else{
-        mensagem_erro.style.display = "flex";
+         mensagem_erro.style.display = "flex";
     }
+
+    // if(user.toUpperCase() == "CARAMICO" && senha == "ipt1234"){
+    //     window.location = "dashboard.html";
+    // } else{
+    //     mensagem_erro.style.display = "flex";
+    // }
 }
