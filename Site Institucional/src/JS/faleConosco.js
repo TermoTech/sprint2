547e445594a149dc3068b7541d52bade6feb46e6
maarcos01 <toretomarcos50@gmail.com>
2023-10-21 10:51:22 -0300

    Adicionei um svg no fale conosco

--- Site Institucional/src/JS/faleConosco.js
+++ Site Institucional/src/JS/faleConosco.js
@@ -2,7 +2,7 @@ function abrirPopUp() {
     fale_conosco.innerHTML = `
     <div class="div-fora">
     <div class="div-pop-up-fc">
-        <br> <u class="margem-header-fc">Fale conosco</u> <img onclick="fecharPopUp()" id="quit_fc" src="../imagens/quitFC.png"><br> <br> <br>
+        <br> <u class="margem-header-fc">Fale conosco</u> <svg onclick="fecharPopUp()" xmlns="http://www.w3.org/2000/svg" style="position: absolute; left: 58%; cursor: pointer;" width="32" height="32" viewBox="0 0 32 32" onclick="fecharAddFunc()"><path fill="currentColor" d="M16 2C8.2 2 2 8.2 2 16s6.2 14 14 14s14-6.2 14-14S23.8 2 16 2zm0 26C9.4 28 4 22.6 4 16S9.4 4 16 4s12 5.4 12 12s-5.4 12-12 12z"/><path fill="currentColor" d="M21.4 23L16 17.6L10.6 23L9 21.4l5.4-5.4L9 10.6L10.6 9l5.4 5.4L21.4 9l1.6 1.6l-5.4 5.4l5.4 5.4z"/></svg><br> <br> <br>
         <form>
         <span class="margem-titulos-fc">Seu email</span> <br>
         <input type="text" id="ipt_email" onchange="validarEmail()" name="email" required> <br> 
