288d9856a04475e0d4a42e9fb21722b55c29ea89
kauanuness <kaua.souza@sptech.school>
2023-11-21 20:08:23 -0300

    Integrando o banco de dados nos gráficos

--- dashboard/src/models/painelModels.js
+++ dashboard/src/models/painelModels.js
@@ -2,10 +2,7 @@ var database = require("../../../database/db.js")
 
 function tempoReal(idUser, fkEmpresa) {
 
-    var instrucao = `
-    SELECT * FROM maquina JOIN sensores ON idMaquina = fkMaquina
-    JOIN empresa ON fkEmpresa = idEmpresa WHERE maquina.fkEmpresa = ${fkEmpresa} ORDER BY sensores.horario DESC LIMIT 4;
-    `;
+    var instrucao = `CALL dadosSensores(${fkEmpresa}, ${idUser});`;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
