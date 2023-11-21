3653ef2c3af1cb0311221102fdbc3fe3e411e362
ClaudioAraujo03 <claudio.araujofo@sptech.school>
2023-11-21 15:07:26 -0300

    Atualizando tabelas do banco de dados

--- Banco de Dados/script_select.sql
+++ Banco de Dados/script_select.sql
@@ -27,3 +27,4 @@ SELECT * FROM usuario_maquina;
 SELECT * FROM sensores JOIN maquina ON fkMaquina = idMaquina;
 SELECT * FROM sensores;
 
+ select * from usuario join ACESSO on idUsuario = fkUsuario join maquina on fkMaquina = idMaquina join empresa on usuario.fkEmpresa = idEmpresa;
