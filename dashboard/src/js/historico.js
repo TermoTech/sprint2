7ea8eca366a5b2b66cc46c85e2aa67f2a13b0124
maarcos01 <toretomarcos50@gmail.com>
2023-11-30 23:13:16 -0300

    criei um filtro para cada parte da maquina

--- dashboard/src/js/historico.js
+++ dashboard/src/js/historico.js
@@ -15,7 +15,7 @@ document.addEventListener("keydown", function (event) {
 
 errosTotais = 0;
 horarios = [];
-parteProcesso = [];
+var parteProcesso = [];
 
 function calcularQuantidadeItens(lista, nome) {
   let quantidade = 0;
@@ -27,12 +27,17 @@ function calcularQuantidadeItens(lista, nome) {
   }
   return quantidade;
 }
-temperaturas = [];
-maquinas = [];
-umidades = [];
+var temperaturas = [];
+var maquinas = [];
+var umidades = [];
 
 function mostrarHistorico() {
   tabela_historico.innerHTML = ``;
+  horarios = [];
+  parteProcesso = [];
+  temperaturas = [];
+  maquinas = [];
+  umidades = [];
 
   filtrarGeral();
 
@@ -50,9 +55,6 @@ function mostrarHistorico() {
         ${temperaturas[i]}
         </th>
         <th>
-        ${umidades[i]}
-        </th>
-        <th>
         ${maquinas[i]}
         </th>
     </tr>
@@ -61,6 +63,8 @@ function mostrarHistorico() {
   }, 2000);
   fecharPopUpFtr();
 }
+
+
 
 function filtrarGeral() {
   var diaInicial = input_dia_inicio.value;
@@ -96,10 +100,10 @@ function filtrarGeral() {
     .then((json) => {
       console.log(JSON.stringify(json));
       for (var i = 0; i < json.length; i++) {
-        horarios.push(json[i].horario);
+        horarios.push(json[i].hora);
         parteProcesso.push(json[i].parteProcesso);
         temperaturas.push(json[i].temperatura);
-        maquinas.push(json[i].fkMaquina);
+        maquinas.push(json[i].maquina);
       }
       // verEmpresa()
     })
@@ -108,52 +112,51 @@ function filtrarGeral() {
       // finalizarAguardar();
     });
 
-  filtarParte();
 }
 
-function filtarParte() {
-  var diaInicial = input_dia_inicio.value;
-  var diaFinal = input_dia_final.value;
-  var maquinaSelecionada = Number(select_maquina.value);
-  var processoSelecionado = select_processo.value;
+// function filtarParte() {
+//   var diaInicial = input_dia_inicio.value;
+//   var diaFinal = input_dia_final.value;
+//   var maquinaSelecionada = Number(select_maquina.value);
+//   var processoSelecionado = select_processo.value;
 
-  fetch(`/supervisorPremium/filtrarGeral`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      inicialDia: diaInicial,
-      finalDia: diaFinal,
-      idMaquina: maquinaSelecionada,
-      parteProcesso: processoSelecionado,
-    }),
-  })
-    .then((respostas) => {
-      if (respostas.ok) {
-        return respostas.json();
-      } else if (respostas.status == 404) {
-        window.alert("Deu 404!");
-      } else {
-        throw (
-          "Houve um erro ao tentar realizar a postagem! Código da resposta: " +
-          respostas.status
-        );
-      }
-    })
-    .then((json) => {
-      console.log(JSON.stringify(json));
-      numero_erro_matriz.innerHTML = calcularQuantidadeItens(
-        parteProcesso,
-        "Processo1"
-      );
-      numero_erro_anel.innerHTML = calcularQuantidadeItens(parteProcesso, "Processo2");
-      numero_erro_reator.innerHTML = calcularQuantidadeItens(parteProcesso, "Processo3");
-      numero_erro_total.innerHTML = errosTotais;
-      // verEmpresa()
-    })
-    .catch(function (resposta) {
-      console.log(`#ERRO: ${resposta}`);
-      // finalizarAguardar();
-    });
-}
+//   fetch(`/supervisorPremium/filtrarGeral`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       inicialDia: diaInicial,
+//       finalDia: diaFinal,
+//       idMaquina: maquinaSelecionada,
+//       parteProcesso: processoSelecionado,
+//     }),
+//   })
+//     .then((respostas) => {
+//       if (respostas.ok) {
+//         return respostas.json();
+//       } else if (respostas.status == 404) {
+//         window.alert("Deu 404!");
+//       } else {
+//         throw (
+//           "Houve um erro ao tentar realizar a postagem! Código da resposta: " +
+//           respostas.status
+//         );
+//       }
+//     })
+//     .then((json) => {
+//       console.log(JSON.stringify(json));
+//       numero_erro_matriz.innerHTML = calcularQuantidadeItens(
+//         parteProcesso,
+//         "Processo1"
+//       );
+//       numero_erro_anel.innerHTML = calcularQuantidadeItens(parteProcesso, "Processo2");
+//       numero_erro_reator.innerHTML = calcularQuantidadeItens(parteProcesso, "Processo3");
+//       numero_erro_total.innerHTML = errosTotais;
+//       // verEmpresa()
+//     })
+//     .catch(function (resposta) {
+//       console.log(`#ERRO: ${resposta}`);
+//       // finalizarAguardar();
+//     });
+// }
