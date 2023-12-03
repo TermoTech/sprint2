1ef6744afee9a5e3b5faa00173af3bdbc40bdc77
ClaudioAraujo03 <claudio.araujofo@sptech.school>
2023-12-03 01:46:12 -0300

    Criando a estrutura do alert

--- dashboard/src/js/alerta.js
+++ dashboard/src/js/alerta.js
@@ -203,3 +203,19 @@ function atualizaProgressBar(min, max, resposta) {
 
 }
 }
+
+
+/* <div class="content-alert"> 
+  <div class="alert">
+    <div class="head-card">
+      <h4>ALERTA ATENÇÃO</h4>
+      <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/></svg>    </div>
+    <div class="body-card-alert">
+      <h5>
+        A máquina x está chegando perto da xxxx limite!!!<br>
+        Medida Registrada: xx°C / x%<br>
+        Parte do processo: xxxxx
+      </h5>
+    </div>
+  </div>
+</div> */
