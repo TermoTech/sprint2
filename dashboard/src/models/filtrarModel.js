7380424b84906e3bef3b9cb3ae77f6ccff02cc63
kauanuness <kaua.souza@sptech.school>
2023-12-04 18:49:28 -0300

    corrigindo erros do html

--- dashboard/src/models/filtrarModel.js
+++ dashboard/src/models/filtrarModel.js
@@ -7,7 +7,7 @@ function filtrarGeral(diaInicial, diaFinal, parteProcesso, idMaquina) {
     var instrucao = `select DATE_FORMAT(c.horario, '%d-%m-%y') as dia, TIME(c.horario) as horario, sensores.localizacao as parteProcesso, c.captura as temperatura, sensores.fkMaquina as maquina FROM captura as c
     JOIN sensores ON c.fkSensor = sensores.idSensor
     JOIN maquina ON sensores.fkMaquina = maquina.idMaquina
-    where c.erro = 1 and c.horario BETWEEN '${diaInicial}' and '${diaFinal}';
+    where c.erro = 1 and c.horario BETWEEN '${diaInicial} 00:00' and '${diaFinal} 23:59';
         `;
   } else if(parteProcesso == "todas" && idMaquina != 0) {
 
@@ -15,7 +15,7 @@ function filtrarGeral(diaInicial, diaFinal, parteProcesso, idMaquina) {
   select DATE_FORMAT(c.horario, '%d-%m-%y') as dia, TIME(c.horario) as horario, sensores.localizacao as parteProcesso, c.captura as temperatura, sensores.fkMaquina as maquina FROM captura as c
   JOIN sensores ON c.fkSensor = sensores.idSensor
   JOIN maquina ON sensores.fkMaquina = maquina.idMaquina  
-  where c.erro = 1 and c.horario BETWEEN '${diaInicial}' and '${diaFinal}' and fkMaquina = ${idMaquina};
+  where c.erro = 1 and c.horario BETWEEN '${diaInicial} 00:00' and '${diaFinal} 23:59' and fkMaquina = ${idMaquina};
       `;
   }
     else if(parteProcesso != "todas" && idMaquina == 0){
@@ -23,14 +23,14 @@ function filtrarGeral(diaInicial, diaFinal, parteProcesso, idMaquina) {
       var instrucao = `select DATE_FORMAT(c.horario, '%d-%m-%y') as dia, TIME(c.horario) as horario, sensores.localizacao as parteProcesso, c.captura as temperatura, sensores.fkMaquina as maquina FROM captura as c
       JOIN sensores ON c.fkSensor = sensores.idSensor
       JOIN maquina ON sensores.fkMaquina = maquina.idMaquina  
-      where c.erro = 1 and c.horario BETWEEN '${diaInicial}' and '${diaFinal}' and sensores.localizacao = '${parteProcesso}'; `
+      where c.erro = 1 and c.horario BETWEEN '${diaInicial} 00:00' and '${diaFinal} 23:59' and sensores.localizacao = '${parteProcesso}'; `
     }
 
     else {
       var instrucao = `select DATE_FORMAT(c.horario, '%d-%m-%y') as dia, TIME(c.horario) as horario, sensores.localizacao as parteProcesso, c.captura as temperatura, sensores.fkMaquina as maquina FROM captura as c
       JOIN sensores ON c.fkSensor = sensores.idSensor
       JOIN maquina ON sensores.fkMaquina = maquina.idMaquina  
-      where c.erro = 1 and c.horario BETWEEN '${diaInicial}' and '${diaFinal}' and sensores.localizacao = '${parteProcesso}' and fkMaquina = ${idMaquina};
+      where c.erro = 1 and c.horario BETWEEN '${diaInicial} 00:00' and '${diaFinal} 23:59' and sensores.localizacao = '${parteProcesso}' and fkMaquina = ${idMaquina};
       ` 
     }
   console.log("Executando a instrução SQL: \n" + instrucao);
